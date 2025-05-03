8af9925395b2676e27ab09acb1892254e44fb31c
Santiago Villanueva <santi00vimu@gmail.com>
2025-05-02 19:45:50 -0600

    modificamos userscontrollers para definir la lógica del crud para los usuarios

--- Backend/controllers/usersControllers.js
+++ Backend/controllers/usersControllers.js
@@ -32,6 +32,59 @@ const generarToken = (id) => {
 
 }
 
+
+const getUsers = asyncHandler( async(req, res) => {
+    const users = await User.find()
+    res.status(200).json({users})
+})
+
+const createUsers = asyncHandler( async(req, res) => {
+    if(!req.body.texto){
+        res.status(400)
+        throw new Error("Favor de insertar un texto")
+    }
+
+    const user = await User.create({
+        texto: req.body.texto
+    })
+    res.status(201).json({user})
+})
+
+const updateUsers = asyncHandler( async(req, res) => {
+
+    const user = await User.findById(req.params.id)
+    if(!tarea) {
+        res.status(404)
+        throw new Error('Usuario no encontrado')
+    }
+    
+    const userUpdated = await User.findByIdAndUpdate(req.params.id, req.body, {
+        new:true,
+    });
+
+    res.status(200).json(userUpdated);
+})
+
+const deleteUsers = asyncHandler( async(req, res) => {
+    const user = await User.findById(req.params.id)
+    if(!user) {
+        res.status(404)
+        throw new Error('Usuario no encontrado')
+    }
+
+    await user.deleteOne()
+
+
+    res.status(200).json({"id": req.params.id})
+})
+
+module.exports = {
+    getTareas,
+    createTareas, 
+    updateTareas, 
+    deleteTareas
+}
+
 module.exports = {
     login
 }

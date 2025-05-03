e7c2d9d1e05c9e383c2bcb54b0d78d26ae7c03f4
Santiago Villanueva <santi00vimu@gmail.com>
2025-05-02 19:33:02 -0600

    ya sirve cotizaciones, ahora intentamos arreglar users

--- Backend/routes/usersRoutes.js
+++ Backend/routes/usersRoutes.js
@@ -3,5 +3,9 @@ const router = express.Router()
 const {login} = require('../controllers/usersControllers')
 
 router.post('/login', login)
+router.get('/', getUsers)
+router.post('/', createUsers)
+router.put('/:id', updateUsers)
+router.delete('/:id', deleteUsers)
 
 module.exports = router

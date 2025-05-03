10beafc1c8f2d528b4114d06ca8c924e6d394d5e
Santiago Villanueva <santi00vimu@gmail.com>
2025-05-02 18:40:44 -0600

    error de server arreglado

--- Backend/models/cotizacionesModels.js
+++ Backend/models/cotizacionesModels.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const userSchema = mongoose.Schema({
+const cotizacionSchema = mongoose.Schema({
     name: {
         type: String,
         required: [true, "Por favor agregue su nombre"],
@@ -19,4 +19,4 @@ const userSchema = mongoose.Schema({
     timestamps : true
 })
 
-module.exports = mongoose.model('Cotizacion', CotizacionSchema)
+module.exports = mongoose.model('Cotizacion', cotizacionSchema)
